ea5da818b5f12772069ede001a42ae040ea8a310
omokpogabriel <omokpogabriel@gmail.com>
2020-10-06 13:49:53 -0700

    added schema defaultlength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\PostRepository;
 use App\Repositories\PostRepositoryInterface;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
